118ab82053c1c48fcc7967161fb92ddafa399755
jurdunnn <jordy.downs@hotmail.co.uk>
2023-05-19 18:38:22 +0100

    Switch to illuminate http instead of curl

--- app/Services/SteamService.php
+++ app/Services/SteamService.php
@@ -3,30 +3,14 @@
 namespace App\Services;
 
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
 
 class SteamService
 {
     public function getGameInfoFromSteam($appid): ?array
     {
-        $url = "https://store.steampowered.com/api/appdetails?appids=$appid";
-
-        $reponse = $this->curlUrl($url);
+        $reponse = Http::get("https://store.steampowered.com/api/appdetails?appids=$appid");
 
         return Arr::flatten(json_decode($reponse, true), 1)[1] ?? null;
-    }
-
-    private function curlUrl(string $url): string
-    {
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $reponse = curl_exec($ch);
-
-        curl_close($ch);
-
-        return $reponse;
     }
 }
