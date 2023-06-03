8da7fc0c5f04ddfd4bcbe2cd16ddc55d938d53c2
jurdunnn <jordy.downs@hotmail.co.uk>
2023-06-03 17:18:23 +0100

    Use Modifier create in metacrtitic action

--- app/Actions/Game/AddMetacriticModifier.php
+++ app/Actions/Game/AddMetacriticModifier.php
@@ -4,50 +4,20 @@ namespace App\Actions\Game;
 
 use App\Enums\ModifierType;
 use App\Models\Game;
+use App\Models\Modifier;
 
 class AddMetacriticModifier
 {
     public function __invoke(Game $game, array $data)
     {
-        if ($game->modifiers()
-            ->where('type', ModifierType::METACRITIC)
-            ->exists()
-        ) {
-            return;
-        }
-
         $score = $data['metacritic']['score'] ?? null;
 
-        if ($score > 70) {
-            $contents = [
-                'title' => 'High Metacritic Score',
-                'type' => ModifierType::METACRITIC,
-                'color' => 'green',
-                'strength' => 10
-            ];
-        } elseif ($score > 50) {
-            $contents = [
-                'title' => 'Average Metacritic Score',
-                'type' => ModifierType::METACRITIC,
-                'color' => 'gray',
-                'strength' => 0
-            ];
-        } elseif ($score === null) {
-            $contents = [
-                'title' => 'No Metacritic Score',
-                'type' => ModifierType::METACRITIC,
-                'color' => 'gray',
-                'strength' => 0
-            ];
-        } else {
-            $contents = [
-                'title' => 'Poor Metacritic Score',
-                'type' => ModifierType::METACRITIC,
-                'color' => 'red',
-                'strength' => -10
-            ];
-        }
-
-        $game->modifiers()->firstOrCreate($contents);
+        Modifier::create($game, ModifierType::METACRITIC, [
+            'positive' => 'High Metacritic Score',
+            'neutral' => 'No Metacritic Score',
+            'negative' => 'Low Metacritic Score',
+        ], function () use ($score) {
+            return $score > 70;
+        });
     }
 }
