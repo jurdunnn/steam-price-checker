6bcfc49c2434958bd6bb20051d43bd8339b2deef
jurdunnn <jordy.downs@hotmail.co.uk>
2023-06-03 17:11:05 +0100

    Use Modifier create for platform action

--- app/Actions/Game/AddPlatformModifier.php
+++ app/Actions/Game/AddPlatformModifier.php
@@ -4,58 +4,26 @@ namespace App\Actions\Game;
 
 use App\Enums\ModifierType;
 use App\Models\Game;
+use App\Models\Modifier;
 
 class AddPlatformModifier
 {
     public function __invoke(Game $game, array $data)
     {
-        if ($game->modifiers()
-            ->where('type', ModifierType::PLATFORM)
-            ->exists()
-        ) {
-            return;
-        }
-
         $platforms = $data['platforms'] ?? null;
 
-        if (!$platforms) {
-            $game->modifiers()->firstOrCreate([
-                'title' => 'No Platform Information',
-                'type' => ModifierType::PLATFORM,
-                'color' => 'gray',
-                'strength' => 0
-            ]);
+        Modifier::create($game, ModifierType::PLATFORM, [
+            'positive' => 'Many Platforms Supported',
+            'neutral' => 'No Platform Information',
+            'negative' => 'Few Platforms Supported',
+        ], function () use ($game, $platforms) {
+            if ($platforms) {
+                $platforms = array_filter($platforms, fn ($platform) => $platform != false);
 
-            return;
-        }
+                return sizeof($platforms) > 2;
+            }
 
-        $platforms = array_filter($platforms, fn ($platform) => $platform != false);
-
-        switch (sizeof($platforms)) {
-            case 1:
-                $game->modifiers()->firstOrCreate([
-                    'title' => 'Few Supported Platforms',
-                    'type' => ModifierType::PLATFORM,
-                    'color' => 'red',
-                    'strength' => -10
-                ]);
-                break;
-            case 2:
-                $game->modifiers()->firstOrCreate([
-                    'title' => 'Average Number of Platforms Supported',
-                    'type' => ModifierType::PLATFORM,
-                    'color' => 'gray',
-                    'strength' => 0
-                ]);
-                break;
-            case 3:
-                $game->modifiers()->firstOrCreate([
-                    'title' => 'Many Supported Platforms',
-                    'type' => ModifierType::PLATFORM,
-                    'color' => 'green',
-                    'strength' => 10
-                ]);
-                break;
-        }
+            return null;
+        });
     }
 }
